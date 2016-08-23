e154193ac9a0ee5dd29d918e8e6a8e7475d52b63
Jerry Jacobs <jerry.jacobs@xor-gate.org>
2016-08-23 22:40:13 +0200

    Blaat

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   sudo apt-get update -qq || true
-  sudo apt-get install -qq -y --no-install-recommends libusb-1.0.0-dev
+  sudo apt-get install -qq -y --no-install-recommends gdb libusb-1.0.0-dev
 else
   brew install libusb
 fi
@@ -11,5 +11,8 @@ if [[ "$CC" == "scan-build" ]]; then
     unset CXX
     scan-build -o analysis --status-bugs make ci
 else
+    lsusb
+    make debug
+    gdb -batch -ex "run" -ex "bt" build/Debug/tests/open_close 2>&1 | grep -v ^"No stack."$
     make ci
 fi
